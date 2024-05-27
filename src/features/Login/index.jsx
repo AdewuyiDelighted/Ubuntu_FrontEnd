8e507bac9ca4096782ff5f9205bd03077b3b0844
delighted <deborahdelighted5@gmail.com>
2024-05-27 08:46:03 +0100

    login integration

--- src/features/Login/index.jsx
+++ src/features/Login/index.jsx
@@ -2,8 +2,32 @@ import style from "./index.module.css"
 
 import images from "../../asset/login/ourCommunity.jpg"
 import FilledButton from "../../components/reuseables/FilledButton";
+import {useState} from "react";
 
 const Login = () => {
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    })
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        // try{
+        //     const response = axios
+        // }
+
+
+
+    }
 
     return (
         <div className={style.main}>
@@ -15,8 +39,10 @@ const Login = () => {
                 <p className={style.login}>Sign in to stay connected with the most prestigious community</p>
 
                 <form className={style.formInfo}>
-                    <input type={"text"} placeholder={"Email:"} className={style.loginInfo}></input>
-                    <input type={"text"} placeholder={"Password:"} className={style.loginInfo}></input>
+                    <input type={"text"} placeholder={"Email:"} className={style.loginInfo}
+                           onChange={(e) => handleChange(e)}></input>
+                    <input type={"text"} placeholder={"Password:"} className={style.loginInfo}
+                           onChange={(e) => handleChange(e)}></input>
                 </form>
                 <div className={style.button}>
                     <FilledButton textColor={"#000000"} backgroundColor={"#3498db"} text={"Login"}/>
